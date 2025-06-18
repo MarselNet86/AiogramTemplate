3dd57f065ffbaf002353107a9f8f611ccd193eff
daimon <schamsutdin88@gmail.com>
2025-06-19 00:19:56 +0500

    добавили env

--- bot/misc/env.py
+++ bot/misc/env.py
@@ -1,6 +1,10 @@
-from os import environ
+from os import getenv
+from dotenv import load_dotenv
 from typing import Final
 
 
+load_dotenv()
+
+
 class TgKeys:
-    TOKEN: Final = environ.get('TOKEN', 'define me!')
+    TOKEN: Final = getenv.get('TOKEN', 'define me!')
