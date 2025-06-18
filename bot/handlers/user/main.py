e8319a8440fc7cfff9fdb4c097b0cd6bd072c6a3
daimon <schamsutdin88@gmail.com>
2025-06-19 00:47:31 +0500

    добавлен обработчик /start

--- bot/handlers/user/main.py
+++ bot/handlers/user/main.py
@@ -1,7 +1,6 @@
 from aiogram import Dispatcher
-
+from bot.handlers.user import register
 
 def register_user_handlers(dp: Dispatcher):
-    # todo: register all user handlers
-    pass
+    dp.include_router(register.router)
 
--- bot/main.py
+++ bot/main.py
@@ -1,3 +1,5 @@
+import logging
+
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.bot import DefaultBotProperties
@@ -15,6 +17,12 @@ async def __on_start_up(dp: Dispatcher) -> None:
 
 
 async def start_bot():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    )
+
     bot = Bot(token=TgKeys.TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
     dp = Dispatcher(storage=MemoryStorage())  # Убрали bot из конструктора Dispatcher
+
     await dp.start_polling(bot, skip_updates=True, on_startup=__on_start_up)
